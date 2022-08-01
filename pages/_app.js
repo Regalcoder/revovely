2d6156c84541bbf02c9a39098a774ec3d069249f
Segun Sodimu <princesegzy01@yahoo.com>
2022-08-01 12:19:02 +0100

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+// import '../styles/globals.css'
+
+// pages/_app.jsx
+import Default from "../layouts/Default";
+// import Layout2 from "@/layouts/Layout2";
+const layouts = {
+  Default: Default,
+  // L2: Layout2,
+};
+const App = ({ Component, pageProps }) => {
+  const Layout = layouts[Component.layout] || ((children) => <>{children}</>);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return (<div>Content Body</div>);
+}
+Home.layout = "Default";
+export default Home;
