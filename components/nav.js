fbf22e853d0c41fc0e7b578aeaf934bb99cf5f0e
Regalcoder <86799597+Regalcoder@users.noreply.github.com>
2022-08-04 16:47:58 +0100

    navbar

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,26 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 const Nav = () => {
-    return ( <div>Nav</div> );
+    return ( 
+        <>
+           <nav className={styles.navContainer} >
+               <img className={styles.logo} src="/logo.png"  height='33px'/>
+               <div className={styles.menuContainer}>
+                    <ul className={styles.menus}>
+                        <li className={styles.menuList}><Link href='/'><a className={styles.menu}>Home</a></Link></li>
+                        <li className={styles.menuList}><Link href=''><a className={styles.menu}>Payments</a></Link></li>
+                        <li className={styles.menuList}><Link href=''><a className={styles.menu}>Bill Payment</a></Link></li>
+                        <li className={styles.menuList}><Link href=''><a className={styles.menu}>Savings</a></Link></li>
+                        <li className={styles.menuList}><Link href=''><a className={styles.menu}>Rewards</a></Link></li>
+                    </ul>
+                    <button className={styles.menuButton}>Get the app</button>
+                </div>
+           </nav>
+        </>
+    
+    
+    );
 }
  
 export default Nav;
